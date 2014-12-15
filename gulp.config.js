6213a8a0754e58944af65bdb808f9ecb05919266
John Papa <john@johnpapa.net>
2014-12-14 19:48:57 -0500

    double to single quotes

--- gulp.config.js
+++ gulp.config.js
@@ -6,44 +6,44 @@ module.exports = (function() {
 
     function getConfig() {
         return {
-            "client": "./src/client/",
-            "server": "./src/server/",
-            "htmltemplates": "./src/client/app/**/*.html",
-            "less": [
-                "./src/client/content/styles.less"
+            'client': './src/client/',
+            'server': './src/server/',
+            'htmltemplates': './src/client/app/**/*.html',
+            'less': [
+                './src/client/content/styles.less'
             ],
-            "js": [
-                "src/client/app/**/*.module.js",
-                "src/client/app/**/*.js",
-                "!src/client/app/**/*.spec.js"
+            'js': [
+                'src/client/app/**/*.module.js',
+                'src/client/app/**/*.js',
+                '!src/client/app/**/*.spec.js'
             ],
-            "specs": [
-                "./src/client/app/**/*.spec.js"
+            'specs': [
+                './src/client/app/**/*.spec.js'
             ],
-            "alljs": [
-                "./src/client/app/**/*.js",
-                "./src/server/**/*.js",
-                "./src/client/test-helpers/**/*.js"
+            'alljs': [
+                './src/client/app/**/*.js',
+                './src/server/**/*.js',
+                './src/client/test-helpers/**/*.js'
             ],
-            "appjs": "src/client/app/**/*.js",
-            "fonts": [
-                "./bower_components/font-awesome/fonts/**/*.*"
+            'appjs': 'src/client/app/**/*.js',
+            'fonts': [
+                './bower_components/font-awesome/fonts/**/*.*'
             ],
-            "images": [
-                "./src/client/content/images/**/*.*"
+            'images': [
+                './src/client/content/images/**/*.*'
             ],
-            "build": "./build/",
-            "temp": "./.tmp/",
-            "report": "./report/",
-            "defaultPort": "7203",
-            "templateCache": {
-                "module": "app.core",
-                "file": "templates.js",
-                "root": "app/"
+            'build': './build/',
+            'temp': './.tmp/',
+            'report': './report/',
+            'defaultPort': '7203',
+            'templateCache': {
+                'module': 'app.core',
+                'file': 'templates.js',
+                'root': 'app/'
             },
-            "bower": {
-                "directory": "./bower_components/",
-                "ignorePath": "../.."
+            'bower': {
+                'directory': './bower_components/',
+                'ignorePath': '../..'
             }
         }
     }
