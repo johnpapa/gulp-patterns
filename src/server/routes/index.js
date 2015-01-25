5aef3b94a8d196b831c550f9926854e5c6e4418e
John Papa <john@johnpapa.net>
2015-01-25 09:56:44 -0500

    added 404 handling to API's in node / express

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,21 +1,61 @@
 module.exports = function(app) {
-    var api = '/api';
+    var api = '/api/';
     var data = '/../../data/';
     var jsonfileservice = require('./utils/jsonfileservice')();
 
-    app.get(api + '/customer/:id', getCustomer);
-    app.get(api + '/customers', getCustomers);
+    app.get(api + 'customer/:id', getCustomer);
+    app.get(api + 'customers', getCustomers);
+
+    app.get(api + '*', notFoundMiddleware);
 
     function getCustomer(req, res, next) {
-        var json = jsonfileservice.getJsonFromFile(data + 'customers.json');
-        var customer = json.filter(function(c) {
-            return c.id === parseInt(req.params.id);
-        });
-        res.send(customer[0]);
+        var id = req.params.id;
+        var msg = 'customer id ' + id + ' not found. ';
+        try {
+            var json = jsonfileservice.getJsonFromFile(data + 'customers.json');
+            var customer = json.filter(function(c) {
+                return c.id === parseInt(id);
+            });
+            if (customer && customer[0]) {
+                res.send(customer[0]);
+            } else {
+                send404(req, res, msg);
+            }
+        }
+        catch (ex) {
+            send404(req, res, msg + ex.message);
+        }
     }
 
     function getCustomers(req, res, next) {
-        var json = jsonfileservice.getJsonFromFile(data + 'customers.json');
-        res.send(json);
+        var msg = 'customers not found. ';
+        try {
+            var json = jsonfileservice.getJsonFromFile(data + 'customers.json');
+            if (json) {
+                res.send(json);
+            } else {
+                send404(req, req, msg);
+            }
+        }
+        catch (ex) {
+            send404(req, res, msg + ex.message);
+        }
+    }
+
+    function notFoundMiddleware(req, res, next) {
+        send404(req, res, 'API endpoint not found');
+    }
+
+    // helper function
+    function send404(req, res, description) {
+        var data = {
+            status: 404,
+            message: 'Not Found',
+            description: description,
+            url: req.url
+        };
+        res.status(404)
+            .send(data)
+            .end();
     }
 };
