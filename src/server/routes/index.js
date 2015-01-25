5b5af6644cb236799c684366dce1ca4656cf1da0
John Papa <john@johnpapa.net>
2015-01-25 13:33:46 -0500

    added the four0four module for 404 handling. api 404's are handled by the routes/index.js. any invalid html template urls from the angular app are handled by the /app/*. any other 404's are routed to the index.html, which handles deep links.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,11 +2,12 @@ module.exports = function(app) {
     var api = '/api/';
     var data = '/../../data/';
     var jsonfileservice = require('./utils/jsonfileservice')();
+    var four0four = require('./utils/404')();
 
     app.get(api + 'customer/:id', getCustomer);
     app.get(api + 'customers', getCustomers);
 
-    app.get(api + '*', notFoundMiddleware);
+    app.get(api + '*', four0four.notFoundMiddleware);
 
     function getCustomer(req, res, next) {
         var id = req.params.id;
@@ -19,11 +20,11 @@ module.exports = function(app) {
             if (customer && customer[0]) {
                 res.send(customer[0]);
             } else {
-                send404(req, res, msg);
+                four0four.send404(req, res, msg);
             }
         }
         catch (ex) {
-            send404(req, res, msg + ex.message);
+            four0four.send404(req, res, msg + ex.message);
         }
     }
 
@@ -34,28 +35,11 @@ module.exports = function(app) {
             if (json) {
                 res.send(json);
             } else {
-                send404(req, req, msg);
+                four0four.send404(req, req, msg);
             }
         }
         catch (ex) {
-            send404(req, res, msg + ex.message);
+            four0four.send404(req, res, msg + ex.message);
         }
-    }
-
-    function notFoundMiddleware(req, res, next) {
-        send404(req, res, 'API endpoint not found');
-    }
-
-    // helper function
-    function send404(req, res, description) {
-        var data = {
-            status: 404,
-            message: 'Not Found',
-            description: description,
-            url: req.url
-        };
-        res.status(404)
-            .send(data)
-            .end();
     }
 };
